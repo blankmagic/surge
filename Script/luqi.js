29ade7bac45a66cb6b8a33e348c2481142e011b5
maicoo <maicoo@live.cn>
2020-04-11 03:05:18 +0800

    Add

---
+++ Script/luqi.js
@@ -0,0 +1,15 @@
+/*
+[Script]
+http-response https:\/\/www\.luqijianggushi\.com\/api\/v2\/user\/get requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/maicoo-l/Surge/master/Script/luqi.js
+
+[MITM]
+hostname = www.luqijianggushi.com
+*/
+
+
+
+
+let obj = JSON.parse($response.body);
+obj.data.is_vip = 1;
+obj.data.vip_endtime = 1630296877;
+$done({body: JSON.stringify(obj)});
