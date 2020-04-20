fdbbb213f28e296f2d3a5ecbe508f35e9607a785
maicoo-l <maicoo@live.cn>
2020-04-20 19:39:02 +0800

    Fix

--- Script/network-changed.js
+++ Script/network-changed.js
@@ -3,13 +3,10 @@ event network-changed by Tempoblink
 version: 2.1
 auther: tempoblink
 [Script]
-Outbound Mode = type=event,event-name=network-changed,debug=1,script-path=JS/network-changed.js
+Outbound Mode = type=event,event-name=network-changed,debug=1,script-path=JS/Network-changed.js
 
 PS:记得自己修改WIFI名称 "home_ssid1or2" 
-主要功能:指定Wi-Fi下,Surge自动化不同出站模式,三个值可选'Direct' or 'Rule' or 'Global-Proxy' 默认 Direct
-虽然设置SSID可以达到基本相同功能
-使用脚本,Surge不会被suspend
-Rewrite和Scripting依然有效
+主要功能:指定Wi-Fi下,Surge自动化不同出站模式,三个值可选'Direct' or 'Rule' or 'Global-Proxy' 
 */
 
 let NETWORK = $network.wifi.ssid;
