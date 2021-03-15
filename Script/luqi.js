4df41d4c0ed2a6167c5ec73b8e1e6498c5bfa92a
maicoo <maicoo@live.cn>
2021-03-16 03:04:18 +0800

    :hammer:

--- Script/luqi.js
+++ Script/luqi.js
@@ -1,6 +1,7 @@
 /*
+This Script By NobyDa
 [Script]
-http-response https:\/\/www\.luqijianggushi\.com\/api\/v2\/user\/get requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/maicoo-l/Surge/master/Script/luqi.js
+Luqi = type=http-response,pattern=https:\/\/www\.luqijianggushi\.com\/api\/v2\/user\/get,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/maicoobox/Surge/master/Script/luqi.js
 
 [MITM]
 hostname = www.luqijianggushi.com
